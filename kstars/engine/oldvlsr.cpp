6b7fc6b95ffdf3ad970060689e550bfc24b17e30
Henry de Valence <hdevalence@hdevalence.ca>
2013-06-12 12:21:08 -0400

    Fix header

--- kstars/engine/oldvlsr.cpp
+++ kstars/engine/oldvlsr.cpp
@@ -1,22 +1,19 @@
-/*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2013  Henry de Valence <hdevalence@hdevalence.ca>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- */
+/***************************************************************************
+     engine/oldvlsr.cpp - deprecated functions for the vlsr calculator.
+                             -------------------
+    begin                : 2013-06-11
+    copyright            : (C) 2013 by Henry de Valence
+    email                : hdevalence@hdevalence.ca
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "oldvlsr.h"
 
