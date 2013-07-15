26899de40acca4f7fef21a5a7c949b9dda33c719
Henry de Valence <hdevalence@hdevalence.ca>
2013-07-14 23:38:34 -0400

    Remove tabs

--- kscl/ksclcontext.cpp
+++ kscl/ksclcontext.cpp
@@ -1,7 +1,7 @@
 /*
  * KStars OpenCL Bindings
- * copyright	: (C) 2013 Henry de Valence
- * email	: hdevalence@hdevalence.ca
+ * copyright    : (C) 2013 Henry de Valence
+ * email        : hdevalence@hdevalence.ca
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
