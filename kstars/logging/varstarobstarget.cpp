553ab1aefdfcbdb04d5ea3a2195093509c97a7a5
Rafal Kulaga <rl.kulaga@gmail.com>
2013-12-28 18:26:15 +0100

    Added VarStarObsTarget class.

---
+++ kstars/logging/varstarobstarget.cpp
@@ -0,0 +1,29 @@
+/***************************************************************************
+                    varstarobstarget.cpp - K Desktop Planetarium
+                             -------------------
+    begin                : Sun Dec 8 2013
+    copyright            : (C) 2013 by Rafal Kulaga
+    email                : rl.kulaga@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "varstarobstarget.h"
+
+using namespace Logging;
+
+VarStarObsTarget::VarStarObsTarget(const int id, const QString &name, const QStringList &aliases, const QString &datasource,
+                                   const int discovererObserverId, const dms &ra, const dms &dec,
+                                   const QString &constellation, const QString &notes, const double apparentMag,
+                                   const QString &classification, const QString &type, const double maxApparentMag,
+                                   const double period) :
+    StarObsTarget(id, name, aliases, datasource, discovererObserverId, ra, dec, constellation, notes, apparentMag,
+                  classification), m_Type(type), m_MaxApparentMag(maxApparentMag), m_Period(period)
+{ }
---
+++ kstars/logging/varstarobstarget.h
@@ -0,0 +1,73 @@
+/***************************************************************************
+                    varstarobstarget.h - K Desktop Planetarium
+                             -------------------
+    begin                : Sun Dec 8 2013
+    copyright            : (C) 2013 by Rafal Kulaga
+    email                : rl.kulaga@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef VARSTAROBSTARGET_H
+#define VARSTAROBSTARGET_H
+
+#include "starobstarget.h"
+
+namespace Logging
+{
+
+class VarStarObsTarget : public StarObsTarget
+{
+public:
+    VarStarObsTarget(const int id, const QString &name, const QStringList &aliases, const QString &datasource,
+                     const int discovererObserverId, const dms &ra, const dms &dec,
+                     const QString &constellation, const QString &notes, const double apparentMag,
+                     const QString &classification, const QString &type, const double maxApparentMag,
+                     const double period);
+
+    QString type() const
+    {
+        return m_Type;
+    }
+
+    double maxApparentMagnitude() const
+    {
+        return m_MaxApparentMag;
+    }
+
+    double period() const
+    {
+        return m_Period;
+    }
+
+    void setType(const QString &type)
+    {
+        m_Type = type;
+    }
+
+    void setMaxApparentMagnitude(const double maxApparentMag)
+    {
+        m_MaxApparentMag = maxApparentMag;
+    }
+
+    void setPeriod(const double period)
+    {
+        m_Period = period;
+    }
+
+private:
+    QString m_Type;
+    double m_MaxApparentMag;
+    double m_Period;
+};
+
+}
+
+#endif // VARSTAROBSTARGET_H
