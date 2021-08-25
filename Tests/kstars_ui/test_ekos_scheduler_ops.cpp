202f9ab837f64c9f7167323c9082a93e2516c9da
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-08-25 18:44:37 +0200

    Correct FileCopyrightText for copyright statements of 'Hy Murveit'

--- Tests/kstars_ui/test_ekos_scheduler_ops.cpp
+++ Tests/kstars_ui/test_ekos_scheduler_ops.cpp
@@ -1,6 +1,5 @@
 /*  KStars scheduler operations tests
-    Copyright (C) 2021
-    Hy Murveit
+    SPDX-FileCopyrightText: 2021 Hy Murveit <hy@murveit.com>
 
     SPDX-License-Identifier: GPL-2.0-or-later
 */
