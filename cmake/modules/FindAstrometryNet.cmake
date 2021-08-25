21a5e8241f504746038995b72019c2ed4fef6f51
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-08-25 22:12:18 +0200

    cmake: Convert "Based on" license statements to SPDX

--- cmake/modules/FindAstrometryNet.cmake
+++ cmake/modules/FindAstrometryNet.cmake
@@ -5,7 +5,9 @@
 #  ASTROMETRYNET_EXECUTABLE - the primary astrometry.net executable
 
 # SPDX-FileCopyrightText: 2016 Jasem Mutlaq <mutlaqja@ikarustech.com>
-# Based on FindLibfacile by Carsten Niehaus, <cniehaus@gmx.de>
+#
+# Based on FindLibfacile:
+# SPDX-FileCopyrightText: Carsten Niehaus, <cniehaus@gmx.de>
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
