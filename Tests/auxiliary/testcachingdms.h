f8ce20606bee8ad8277c3f223825c90a8e426334
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-08-25 08:23:16 +0200

    Put copyright statement and license text in the same block
    This way licensedigger will correctly identify it as the license header and produce better formatting

--- Tests/auxiliary/testcachingdms.h
+++ Tests/auxiliary/testcachingdms.h
@@ -3,9 +3,6 @@
                              -------------------
     begin                : Sun 25 Sep 2016 03:56:35 CDT
     copyright            : (C) 2016 by Akarsh Simha <akarsh.simha@kdemail.net>
-***************************************************************************/
-
-/***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
--- kstars/auxiliary/trixelcache.h
+++ kstars/auxiliary/trixelcache.h
@@ -3,9 +3,6 @@
                              -------------------
     begin                : Jun 2021
     copyright            : (C) 2021 by Valentin Boettcher, Jason Harris <hiro at protagon.space; @hiro98:tchncs.de>
-***************************************************************************/
-
-/***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
