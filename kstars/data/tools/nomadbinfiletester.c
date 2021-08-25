f6d194da53df9949b85327f1ec83f586395c815e
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-08-25 19:19:18 +0200

    Clean up unneeded statements  in license headers

--- kstars/data/tools/nomadbinfiletester.c
+++ kstars/data/tools/nomadbinfiletester.c
@@ -1,8 +1,5 @@
 
 /***************************************************************************
-     nomadbinfiletester.c - Check star catalog (binary) files for errors
-                             -------------------
-    begin                : Tue Jul 15 2008
     SPDX-FileCopyrightText: 2008 Akarsh Simha <akarshsimha@gmail.com>
  *                                                                         *
  *   SPDX-License-Identifier: GPL-2.0-or-later
