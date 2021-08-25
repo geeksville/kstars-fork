cc67ebd8243dd976d1abae321b6531bbda9f093b
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-08-25 19:05:40 +0200

    Improve formatting of license headers

--- kstars/skymapqdraw.cpp
+++ kstars/skymapqdraw.cpp
@@ -1,11 +1,8 @@
 /*
     SPDX-FileCopyrightText: 2010 Akarsh Simha <akarsh.simha@kdemail.net>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
 */
-/***************************************************************************
- *                                                                         *
- *   SPDX-License-Identifier: GPL-2.0-or-later
- *                                                                         *
- ***************************************************************************/
 
 #include "skymapqdraw.h"
 #include "skymapcomposite.h"
