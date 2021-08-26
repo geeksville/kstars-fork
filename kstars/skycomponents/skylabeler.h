be50da525541f50a08d6cf55fc6abe6d6699dc77
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-08-26 18:42:18 +0200

    Manually convert GPL-2.0-or-later license statements

--- kstars/skycomponents/skylabeler.h
+++ kstars/skycomponents/skylabeler.h
@@ -1,19 +1,7 @@
 /*
-    skylabeler.h  -  K Desktop Planetarium
-    -------------------
-    begin				: 2007-07-10
-    SPDX-FileCopyrightText: 2007 James B. Bowlin
-    email				: bowlin@mindspring.com
+    SPDX-FileCopyrightText: 2007 James B. Bowlin <bowlin@mindspring.com>
+    SPDX-License-Identifier: GPL-2.0-or-later
 */
-
-/***************************************************************************
- *																		 *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or	 *
- *   (at your option) any later version.								   *
- *																		 *
- ***************************************************************************/
 
 #pragma once
 
