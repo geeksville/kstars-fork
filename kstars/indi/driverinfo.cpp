c82cde8540777a08468705483ee1c66813acaac3
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-08-27 07:59:56 +0200

    Put contact information in <> instead of ()
    This is the SPDX standard

--- kstars/indi/driverinfo.cpp
+++ kstars/indi/driverinfo.cpp
@@ -1,5 +1,5 @@
 /*
-    SPDX-FileCopyrightText: 2012 Jasem Mutlaq (mutlaqja@ikarustech.com)
+    SPDX-FileCopyrightText: 2012 Jasem Mutlaq <mutlaqja@ikarustech.com>
 
     SPDX-License-Identifier: GPL-2.0-or-later
 */
