f184119f9b0ab9826bd2f1130416e8e35391ef82
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-08-27 07:59:56 +0200

    Put contact information in <> instead of ()
    This is the SPDX standard

--- kstars/ekos/indihub.h
+++ kstars/ekos/indihub.h
@@ -1,5 +1,5 @@
 /*
-    SPDX-FileCopyrightText: 2020 Jasem Mutlaq (mutlaqja@ikarustech.com)
+    SPDX-FileCopyrightText: 2020 Jasem Mutlaq <mutlaqja@ikarustech.com>
 
     SPDX-License-Identifier: GPL-2.0-or-later
 */
