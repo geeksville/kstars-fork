ead437df24575f1d91dd60b89053cd65d1777b81
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-08-27 07:59:56 +0200

    Put contact information in <> instead of ()
    This is the SPDX standard

--- kstars/fitsviewer/hough/houghline.cpp
+++ kstars/fitsviewer/hough/houghline.cpp
@@ -1,5 +1,5 @@
 /*
-    SPDX-FileCopyrightText: 2019 Patrick Molenaar (pr_molenaar@hotmail.com)
+    SPDX-FileCopyrightText: 2019 Patrick Molenaar <pr_molenaar@hotmail.com>
 
     SPDX-License-Identifier: GPL-2.0-or-later
 */
