KDE/kstars

commit f9839263ad14a9a589a102fbb7dfa6e5b7501da0

Fix issue where the wait time for local or remote drivers is not sufficient due to pre and post delays. Now we account for them to prevent premature timeout warnings.

kstars/ekos/manager.cpp
@@ -1186,7 +1186,27 @@ void Manager::setClientStarted(const QString &host, int port)
         }
     }
 
-    QTimer::singleShot(MAX_LOCAL_INDI_TIMEOUT, this, &Ekos::Manager::checkINDITimeout);
+    auto maxTimeout = MAX_LOCAL_INDI_TIMEOUT;
+
+    // Parse script, if any
+    QJsonParseError jsonError;
+    QJsonArray profileScripts;
+    QJsonDocument doc = QJsonDocument::fromJson(m_CurrentProfile->scripts, &jsonError);
+
+    // If we have any rules that delay startup of drivers, we need to take that into account
+    // otherwise Ekos would prematurely declare that drivers failed to connect.
+    if (jsonError.error == QJsonParseError::NoError)
+    {
+        profileScripts = doc.array();
+        for (const auto &oneRule : qAsConst(profileScripts))
+        {
+            auto totalDelay = (oneRule["PreDelay"].toDouble(0) + oneRule["PostDelay"].toDouble(0)) * 1000;
+            if (totalDelay >= maxTimeout)
+                maxTimeout = totalDelay + MAX_LOCAL_INDI_TIMEOUT;
+        }
+    }
+
+    QTimer::singleShot(maxTimeout, this, &Ekos::Manager::checkINDITimeout);
 }
 
 void Manager::setClientFailed(const QString &host, int port, const QString &errorMessage)

geeksville/kstars-fork

commit 92eb37bdb3e24bd06e6da9977f3bf76218c95339

Fix access by key on QJsonValue
Fixes: f9839263ad14a9a589a102fbb7dfa6e5b7501da0

kstars/ekos/manager.cpp
@@ -1200,7 +1200,8 @@ void Manager::setClientStarted(const QString &host, int port)
         profileScripts = doc.array();
         for (const auto &oneRule : qAsConst(profileScripts))
         {
-            auto totalDelay = (oneRule["PreDelay"].toDouble(0) + oneRule["PostDelay"].toDouble(0)) * 1000;
+            const auto &oneRuleObj = oneRule.toObject();
+            auto totalDelay = (oneRuleObj["PreDelay"].toDouble(0) + oneRuleObj["PostDelay"].toDouble(0)) * 1000;
             if (totalDelay >= maxTimeout)
                 maxTimeout = totalDelay + MAX_LOCAL_INDI_TIMEOUT;
         }